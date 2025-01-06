5b0713ad18ad773f825b615bd8e3a04bc47e0f5f
Adityaverma008 <119110181+Adityaverma008@users.noreply.github.com>
2025-01-06 16:18:56 +0530

    Add files via upload

---
+++ portfolio/postfolio.js
@@ -0,0 +1,12 @@
+// const modeToggle = document.getElementById('mode-toggle');
+// const theme = document.getElementById('theme');
+
+// modeToggle.addEventListener('click', () => {
+//     if (theme.getAttribute('href') === 'styles.css') {
+//         theme.href = 'dark-mode.css';
+//         document.body.classList.add('dark-mode');
+//     } else {
+//         theme.href = 'styles.css';
+//         document.body.classList.remove('dark-mode');
+//     }
+// });
